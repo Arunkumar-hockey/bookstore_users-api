58cd36d6be5e07f237ae45a1a66862eb126af231
Arun Kumar K <83031170+Arunkumar-hockey@users.noreply.github.com>
2022-01-05 18:41:58 +0530

    test

--- app/application.go
+++ app/application.go
@@ -1,6 +1,8 @@
 package app
 
 import (
+	"userapi/logger"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -8,5 +10,6 @@ var router = gin.Default()
 
 func StartApplication() {
 	mapUrls()
+	logger.Info("about to start the application...")
 	router.Run(":8080")
 }
